f7a65933f3b66551be43aad6068d8f60295d48e0
hsh1105 <hantg1105@naver.com>
2024-05-02 09:20:27 +0900

    Requirements 1

--- src/App.java
+++ src/App.java
@@ -4,10 +4,9 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        int first = sc.nextInt();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int first = sc.nextInt(); // 첫번째 번호 입력
+
         System.out.print("두 번째 숫자를 입력하세요: ");
-        int second = sc.nextInt();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int second = sc.nextInt(); // 두번째 번호 입력
     }
 }
