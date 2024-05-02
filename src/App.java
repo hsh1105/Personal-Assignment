9655950008745aa4ccb1bcfb1dce16c740b349aa
hsh1105 <hantg1105@naver.com>
2024-05-02 09:36:59 +0900

    Requirements 3

--- src/App.java
+++ src/App.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        int result = 0; // 정답 변수 선언
         Scanner sc = new Scanner(System.in);
+
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int first = sc.nextInt(); // 첫번째 번호 입력
 
@@ -11,5 +13,30 @@ public class App {
 
         System.out.print("두 번째 숫자를 입력하세요: ");
         int second = sc.nextInt(); // 두번째 번호 입력
+
+        switch (symbol) {
+            case '+':
+                result = first + second;
+                System.out.println("결과 : "+result);
+                break;
+            case '-':
+                result = first - second;
+                System.out.println("결과 : "+result);
+                break;
+            case '*':
+                result = first * second;
+                System.out.println("결과 : "+result);
+                break;
+            case '/':
+                if (second == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = first / second;
+                System.out.println("결과 : "+result);
+                break;
+            default:
+                System.out.println("올바른 연산기호를 입력해주세요");
+        }
     }
 }
